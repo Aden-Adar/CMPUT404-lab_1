9f97abd764ce21147a03ccb0c34bb19537373644
Aden-Adar <77550086+Aden-Adar@users.noreply.github.com>
2023-01-11 17:52:57 -0700

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,5 +1,5 @@
 import requests
 
 print(requests.__version__)
-response = requests.get("https://google.com")
+response = requests.get("https://raw.githubusercontent.com/Aden-Adar/CMPUT404-lab_1/main/lab1.py")
 print(response.text)
